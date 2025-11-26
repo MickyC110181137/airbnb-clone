985b3b7c7c62d608a9397ecf71aca44c97c28f49
MickyC110181137 <1209436216micky@gmail.com>
2025-11-26 17:38:14 +0800

    leaflet image(path)

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,36 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: [
+//       "avatars.githubusercontent.com",
+//       "lh3.googleusercontent.com",
+//       "res.cloudinary.com",
+//     ],
+//   },
+// };
+
+// export default nextConfig;
